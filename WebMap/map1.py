9336b870c5c13e3625232d5327b636119cde2e69
tianlu <helloluckytea@gmail.com>
2020-06-18 01:03:21 +1000

    webMap with Folium

---
+++ WebMap/map1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 18 00:22:21 2020
+
+@author: tianlu
+"""
+
+
+import folium
+import pandas
+
+map = folium.Map(location=[38.58, -99.09],zoom_start=6,tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My Map")
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+name = list(data["NAME"])
+elev = list(data["ELEV"])
+
+def iconColor(el):
+    if el<1000:
+        return 'green'
+    elif 1000<=el<3000:
+        return 'orange'
+    else:
+        return 'red'
+
+for lt,ln,nm,el in zip(lat,lon,name,elev):
+    fg.add_child(folium.Marker(location=[lt,ln],popup=str(nm)+" "+str(el),icon=folium.Icon(color=iconColor(el))))
+
+map.add_child(fg)
+map.save("Map1.html")
